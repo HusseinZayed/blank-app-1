051dddbf86bb6e8497e875f4508b5ebd714a3a37
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-08-23 11:38:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ with open('modell.pkl', 'rb') as file:
     model = pickle.load(file)
 
 
-# Check the number of features the model expects
-n_features = model.n_features_in_
+
 
 # Streamlit App
 st.title("Linear Regression Prediction")
@@ -23,13 +22,8 @@ feature_3 = st.sidebar.number_input("Feature 3", value=3.0)
 # Prepare the input data
 input_data = np.array([[feature_1, feature_2, feature_3]])
 
-# Check if the input data has the correct number of features
-if input_data.shape[1] != n_features:
-    st.error(f"The model expects {n_features} features, but received {input_data.shape[1]} features.")
-else:
-    # Make prediction
-    prediction = model.predict(input_data)
+prediction = model.predict(input_data)
     
-    # Display the prediction
-    st.write("### Predicted Output")
-    st.write(prediction[0])
+# Display the prediction
+st.write("### Predicted Output")
+st.write(prediction[0])
