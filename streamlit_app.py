60ba984c661d6c06e82229c2b425196d3afd803a
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-08-23 11:39:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,21 @@
 
 import streamlit as st
 import pickle
-import numpy as np
 
 # load pkl file
-with open('modell.pkl', 'rb') as file:
+with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
+#title the page
+st.title("predict the CO2 EMISSIONS of car")
 
 
+#inputs
+engine_size = st.number_input('Engine Size' , min_value=0.0 , max_value=10.0,value=1.0)
+cylinder = st.number_input('Cylinder' , min_value=0.0 , max_value=10.0,value=1.0)
+fuel_consumption =  st.number_input('Fuel Consumption' , min_value=0.0 , max_value=100.0,value=1.0)
 
-# Streamlit App
-st.title("Linear Regression Prediction")
+output = model.predict([[engine_size,cylinder,fuel_consumption]])
 
-# Input features
-st.sidebar.header("Input Features")
-feature_1 = st.sidebar.number_input("Feature 1", value=1.0)
-feature_2 = st.sidebar.number_input("Feature 2", value=2.0)
-feature_3 = st.sidebar.number_input("Feature 3", value=3.0)
-
-# Prepare the input data
-input_data = np.array([[feature_1, feature_2, feature_3]])
-
-prediction = model.predict(input_data)
-    
-# Display the prediction
-st.write("### Predicted Output")
-st.write(prediction[0])
+#display the result
+st.write("the predict CO2 of car is : ",output[0])
