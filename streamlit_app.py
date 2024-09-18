98877db532a5eabdd24c6a596f56f4c00da7fddc
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 20:56:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ if file is not None:
   st.write(df)
 
 num_row = st.slider('choose num rows',min_value=1,max_value=len(df),step=1)
-st.write(df[:num_row])
+names_column = st.multiselect('choose names of columns',df.columns.to_list())
+st.write(df[:num_row][names_column])
