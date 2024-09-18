851bcb0a3cff99f9e51d0631c52928260501f7ed
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 21:12:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+# app 2
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.header('file upload app 2')
 file = st.file_uploader('upload dataset',type=['csv'])
@@ -9,7 +11,13 @@ if file is not None:
 
 num_row = st.slider('choose num rows',min_value=1,max_value=len(df),step=1)
 names_column = st.multiselect('choose names of columns',df.columns.to_list())
+st.write(df[:num_row][names_column])
+
 if names_column:
   st.write(df[:num_row][names_column])
 else:
   st.write(df[:num_row])
+
+
+fig = px.scatter(df,x='population',y='total_rooms')
+st.plotly_chart(fig)
