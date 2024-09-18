800a2c9d07287f6128d9634930e23af92a1200c4
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 16:07:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,4 @@
 ## example 1 with sidebar and spinner
 import streamlit as st
-
-import time
-st.sidebar.title('Sidebar')
-
-st.header('Calculate Area')
-
-with st.sidebar:
-  choose = st.selectbox('Choose the shape',['Circle','Rectangle'])
-
-area =None
-
-if choose == 'Circle':
-  r = st.number_input('Enter the radius',min_value=1,max_value=50)
-  area = r*r*3.14
-elif choose == 'Rectangle':
-  l = st.number_input('Enter the length',min_value=1,max_value=50)
-  w = st.number_input('Enter the width',min_value=1,max_value=50)
-  area = l*w
-
-btn = st.button('Calculate')
-if btn:
-  with st.spinner('Calculating...'):
-    time.sleep(2)
-  st.write(f'The area is {area}')
+st.header('File Upload')
+uploaded_file = st.file_uploader('Choose a file')
