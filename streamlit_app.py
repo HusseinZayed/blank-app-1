3381647237b8306a5ebf2b6ea5c4c2e53bc65a85
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 16:37:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ uploaded_file = st.file_uploader('Choose a file',type=['csv'])
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
-n_rows = st.slider('Enter the number of rows',min_value=1,max_value=len(df),step=1)
+n_rows = st.slider('Enter the number of rows',min_value=1,max_value=len(df),step=1,value=1)
 st.write(df.head(n_rows))
 
 
