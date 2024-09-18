eed6299b58ae6d0393ab84fd28e11ab0874260d9
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 17:04:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+## add filter and divid it as columns
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -8,15 +10,20 @@ uploaded_file = st.file_uploader('Choose a file',type=['csv'])
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
-  n_rows = st.slider('Enter the number of rows',min_value=1,max_value=len(df),step=1)
-  column_choose = st.multiselect('Choose the columns',df.columns.to_list())
+n_rows = st.slider('Enter the number of rows',min_value=1,max_value=len(df),step=1)
+column_choose = st.multiselect('Choose the columns',df.columns.to_list())
 
-  if column_choose:
-    st.write(df[:n_rows][column_choose])
-  else:
-    st.write(df[:n_rows])
+if column_choose:
+  st.write(df[:n_rows][column_choose])
+else:
+  st.write(df[:n_rows])
+  
+################################
 
-fig1 = px.scatter(df,x='population',y='households')
+num_col = df.select_dtypes(include='number').columns.to_list()
+
+x_col = st.selectbox('Choose the x axis',num_col)
+y_col = st.selectbox('Choose the y axis',num_col)
+
+fig1 = px.scatter(df,x=x_col,y=y_col)
 st.plotly_chart(fig1)
-
-
