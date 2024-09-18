ac4602a42481b66b2a77740be2856b836579d995
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 22:28:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ item = st.text_input('add item')
 btn = st.button('add')
 
 if btn:
-  session_state.fuirts.append(item)
+  st.session_state.fuirts.append(item)
 
-st.write(fuirts)
+st.write(st.session_state.fuirts)
