d19dae276c0f0311a0570a0cdf8c0e48307e4b56
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 20:51:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
+
 st.header('file upload app 2')
 file = st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
   df = pd.read_csv(file)
   st.write(df)
+
+num_row = st.slider('choose num rows',min_value=1,max_value=len(df),step=1)
+st.write(df[:num_row])
