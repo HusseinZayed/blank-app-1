738cfa7999d3f0fd42b5295ac21e7bec62752b62
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 16:11:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 ## example 1 with sidebar and spinner
 import streamlit as st
+import pandas as pd
 st.header('File Upload')
-uploaded_file = st.file_uploader('Choose a file')
+uploaded_file = st.file_uploader('Choose a file',type=['csv'])
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write(df)
