0967771ca6a5a469f416f98132e0e837ea90c819
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 17:39:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,14 @@
-## add filter and divid it as columns
-
-import streamlit as st
-## with session_state
-# application1 3
 import streamlit as st
 st.header('Session State')
 
-if 'text_list' not in st.session_state:
-  st.session_state.text_list = []
+text_list = []
 # Text input
 user_input = st.text_input('Enter some text')
 
 # Buttons
 if st.button('Append'):
-  st.session_state.text_list.append(user_input)
+  text_list.append(user_input)
   
 
 # Display the list
-st.write('Text list:', st.session_state.text_list)
+st.write('Text list:',text_list)
