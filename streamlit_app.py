1364aa3198e65bb64c1c61feeba86012499411e1
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 16:34:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-#import plotly.express as px
 
 st.header('File Upload')
 uploaded_file = st.file_uploader('Choose a file',type=['csv'])
@@ -9,11 +8,6 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
 n_rows = st.slider('Enter the number of rows',min_value=1,max_value=len(df),step=1)
-column_choose = st.multiselect('Choose the columns',df.columns.to_list())
-
-if column_choose:
-  st.write(df[:n_rows][column_choose])
-else:
-  st.write(df[:n_rows])
+st.write(df.head(n_rows))
 
 
