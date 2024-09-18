cdc5ee3ea8722ff943588ae6ad0cc53c90dd4be9
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-18 22:07:19 +0300

    Update 1_calculate.py

--- pages/1_calculate.py
+++ pages/1_calculate.py
@@ -1,1 +1,26 @@
+# app 1
+import streamlit as st
+import time
+st.sidebar.title('Sidebar')
+area = None
+st.header("calculate Area")
+
+with st.sidebar:
+  choose = st.selectbox('choose the shape',['circle','rectangle'])
+
+
+if choose=='circle':
+  r = st.number_input('enter the radius',min_value=1,max_value=100)
+  area = r*r*3.14
+elif choose=='rectangle':
+  w = st.number_input('enter the width',min_value=1,max_value=100)
+  h = st.number_input('enter the highet',min_value=1,max_value=100)
+  area = h*w
+
+btn = st.button('calculate')
+
+if btn:
+  with st.spinner('loading....'):
+    time.sleep(2)
+  st.write(f'the area is {area}')
 
